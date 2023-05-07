b8bb41b42c508b110488415de9d4fa940521c04d
Fede-sa <cr.sanchezfederico@gmail.com>
2023-05-07 02:30:19 -0300

    commit completo con persona

--- Back/porfolio/src/main/java/com/proyectofinal/porfolio/controller/Controller.java
+++ Back/porfolio/src/main/java/com/proyectofinal/porfolio/controller/Controller.java
@@ -1,27 +1,48 @@
 package com.proyectofinal.porfolio.controller;
 
+import com.proyectofinal.porfolio.model.Persona;
+import com.proyectofinal.porfolio.service.IPersonaService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http:/localhost:4200")
 public class Controller {
     
-    @GetMapping("/hola/{nombre}/{apellido}")
-    public String decirHola(@PathVariable String nombre,
-                            @PathVariable String apellido){
-        return "Hola "+nombre+", "+apellido+" venis mejor!!!!";
-    } 
+    @Autowired
+    private IPersonaService persoServ;
     
-    @GetMapping("/chau")
-    public String decirChau(@RequestParam String nombre,
-                            @RequestParam String apellido,
-                            @RequestParam String texto){
-     return "chau mundo, " + nombre+", "+apellido+" "+texto;   
+    @PostMapping("/crear/persona")
+    public void agregarPersona(@RequestBody Persona per){
+        persoServ.crearPersona(per);
     }
-   
-}   
+        
+    @GetMapping("/ver/personas")
+    @ResponseBody
+    public List<Persona> verPersonas(){
+            return persoServ.verPersonas();
+        }
+    
+    @DeleteMapping("/borrar/{id}")
+    public void elimPersona(@PathVariable Long id){
+        persoServ.borrarPersona(id);
+    }
+     
+    @PutMapping("/editar")
+    public void editPersona(@RequestBody Persona per){
+        persoServ.editarPersona(per);
+    }
+    
+}
+
 
     
--- Back/porfolio/src/main/java/com/proyectofinal/porfolio/model/Persona.java
+++ Back/porfolio/src/main/java/com/proyectofinal/porfolio/model/Persona.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,13 +19,15 @@ public class Persona {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     
+    @NotNull
+    @Size(min = 1, max=50, message="No cumple con la longitud")
     private String nombre;
+    
+    @NotNull
+    @Size(min = 1, max=50, message="No cumple con la longitud")
     private String apellido;
     
+    @Size(min = 1, max=50, message="No cumple con la longitud")
+    private String img;
     
-    public Persona(Long id, String nombre, String apellido){
-        this.id = id;
-        this.nombre = nombre;
-        this.apellido = apellido;
-    }
 }
